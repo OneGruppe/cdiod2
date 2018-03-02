8ced2090621089f8999a6fda692881ecd158c006
Niklaes Jacobsen <niklaes@gmail.com>
2018-03-02 21:27:49 +0100

    Exceptions with Weight-message

---
+++ src/functionality/WeightException.java
@@ -0,0 +1,12 @@
+package functionality;
+
+public class WeightException extends Exception {
+
+	private static final long serialVersionUID = 68288822287491631L;
+	public WeightException() {
+		super();
+	}
+	public WeightException(String msg) {
+		super(msg);
+	}
+}
--- src/functionality/Weight_Translate.java
+++ src/functionality/Weight_Translate.java
@@ -10,26 +10,30 @@ public class Weight_Translate {
 	 * Viser en besked på display
 	 * @param message Den besked der vises på UI.
 	 */
-	public void showMsg(String message) {	
+	public void showMsg(String message) throws WeightException{	
+		
 	}
 
 	/**
 	 * Viser en lang besked på display
 	 * @param message Den besked der vises på UI.
 	 */
-	public void showLongMsg(String message) {	
+	public void showLongMsg(String message) throws WeightException{	
+		
 	}
 
 	/**
 	 * Fjerner beskeden fra display
 	 */
-	public void removeMsg() {
+	public void removeMsg() throws WeightException{
+		
 	}
 
 	/**
 	 * Skriver en besked til UI, og får en besked tilbage fra vægt.
 	 */
-	public String getInputWithMsg(String message) {
+	public String getInputWithMsg(String message) throws WeightException{
+		
 		return null;
 	}
 
@@ -37,7 +41,8 @@ public class Weight_Translate {
 	 * Trækker information om nuværende vægt-info
 	 * @return vægt i double
 	 */
-	public double getWeight() {
+	public double getWeight() throws WeightException{
+		
 		return 0;
 	}
 
@@ -45,7 +50,8 @@ public class Weight_Translate {
 	 * Trækker information om nuværende Tara-vægt
 	 * @return Tara vægt i double
 	 */
-	public double getTaraWeight() {
+	public double getTaraWeight() throws WeightException{
+		
 		return 0;
 	}
 }

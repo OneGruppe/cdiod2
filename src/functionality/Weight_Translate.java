9149f9c5d44e8ba9093baacae2fa365383b5e98a
Sebastian <s170423@student.dtu.dk>
2018-03-03 13:34:24 +0100

    open telnet possible

--- src/functionality/Weight_Translate.java
+++ src/functionality/Weight_Translate.java
@@ -1,66 +1,101 @@
 package functionality;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.Socket;
 
 public class Weight_Translate {
-	private Socket sock;
+
+	// declare socket to open connection to TCP/Telnet
+	// declare Writer and reader for I/O
+	private Socket socket;
+	private PrintWriter write;
+	private BufferedReader read;
+
 	// ******************************************
 	// Denne klasse har direkte adgang til vægten
 	// ******************************************
-	public Weight_Translate(){
-		try {
-			sock = new Socket ("127.0.0.1", 8000);
-		}
-		catch(Exception e) {
 
+	public Weight_Translate() throws IOException {
+
+		try {
+			// initialize the socket with the local IP-addr and right port
+			socket = new Socket("127.0.0.1", 8000);
+
+			// initialize the writer and the reader with the socket output and input stream
+			write = new PrintWriter(socket.getOutputStream(), true);
+			read = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+			// catch of exceptions
+		} catch (IOException e) {
+			return;
 		}
+
 	}
-	
+
 	/**
 	 * Viser en besked på display
-	 * @param message Den besked der vises på UI.
+	 * 
+	 * @param message
+	 *            Den besked der vises på UI.
+	 * @throws IOException 
 	 */
-	public void showMsg(String message) throws WeightException{	
-		
+	public void showMsg(String message) throws WeightException, IOException {
+		// write commands to the weight (open telnet)
+		write.println("D Burhan");
+
+		// read the response of the weight
+		System.out.println(read.readLine());
+
+		// insure leak and close everything
+		write.close();
+		read.close();
+		socket.close();
 	}
 
 	/**
 	 * Viser en lang besked på display
-	 * @param message Den besked der vises på UI.
+	 * 
+	 * @param message
+	 *            Den besked der vises på UI.
 	 */
-	public void showLongMsg(String message) throws WeightException{	
+	public void showLongMsg(String message) throws WeightException {
 
 	}
 
 	/**
 	 * Fjerner beskeden fra display
 	 */
-	public void removeMsg() throws WeightException{
+	public void removeMsg() throws WeightException {
 
 	}
 
 	/**
 	 * Skriver en besked til UI, og får en besked tilbage fra vægt.
 	 */
-	public String getInputWithMsg(String message) throws WeightException{
+	public String getInputWithMsg(String message) throws WeightException {
 
 		return null;
 	}
 
 	/**
 	 * Trækker information om nuværende vægt-info
+	 * 
 	 * @return vægt i double
 	 */
-	public double getWeight() throws WeightException{
+	public double getWeight() throws WeightException {
 
 		return 0;
 	}
 
 	/**
 	 * Trækker information om nuværende Tara-vægt
+	 * 
 	 * @return Tara vægt i double
 	 */
-	public double getTaraWeight() throws WeightException{
+	public double getTaraWeight() throws WeightException {
 
 		return 0;
 	}
--- src/main/Main.java
+++ src/main/Main.java
@@ -1,12 +1,11 @@
 package main;
 
-import functionality.Functionality;
+import java.io.IOException;
 
 public class Main {
 
-	public static void main(String[] args) {
-		Functionality f = new Functionality();
+	public static void main(String[] args) throws IOException {
+		// Functionality f = new Functionality();
 		// System.out.println("Programmet virker, alt der ikke virker = feature");
 	}
-
 }

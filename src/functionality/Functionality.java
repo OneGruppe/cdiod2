e7d2ef86de3fceaed7601a02131a98d0b530f84d
Burhan27 <32563710+Burhan27@users.noreply.github.com>
2018-03-08 16:31:54 +0100

    i did this

--- src/functionality/Functionality.java
+++ src/functionality/Functionality.java
@@ -7,12 +7,12 @@ import data.*;
 public class Functionality {
 	private ObjectTransfer d;
 	private Weight_Translate w;
-	
+
 	public Functionality(){
 		d = new ObjectTransfer();
 		w = new Weight_Translate();
 	}
-	
+
 	/**
 	 * Kører flow for vægten.
 	 */
@@ -22,76 +22,78 @@ public class Functionality {
 
 			// 1 - Vægten beder om, at der indtastes operatørnummer
 			// 2 - Operatøren indtaster sit brugernummer (område 11-99)
-				int userInput = w.getInputWithMsg("Indtast operatørnummer");
-					w.showLongMsg(d.userInDatabase(userInput).getName());
-					
-					// 3 - Operatørens navn findes i databasen og vises på vægten
-					
-					// 4 - Operatøren kvitterer for at navnet er korrekt
-					TimeUnit.SECONDS.sleep(3);
-					w.getInputWithMsg("Indtast 1, for at kvittere.");
-					
-					// 5 - Vægten beder om, at der indtastes batch nummer (område 1000-9999)
-					
-					// 6 - Operatør indtaster batch nummer
-					
-					// 7 - Operatøren instrueres om, at vægten skal være ubelastet
-					
-					// 8 - Operatøren kvitterer
-					TimeUnit.SECONDS.sleep(3);
-					w.getInputWithMsg("Indtast 1, for at kvittere.");
-					
-					// 9 - Vægten tareres
-					w.setTaraWeight();
-					
-					// 10 - Operatøren instrueres om, at placere tara (tom beholder)  på vægten7
-					w.showLongMsg("Placer en tom beholder på vægten");
-					
-					// 11 - Operatøren kvitterer
-					TimeUnit.SECONDS.sleep(3);
-					w.getInputWithMsg("Indtast 1, for at kvittere.");
-					
-					// 12 - Tara’s vægt registreres
-					d.batchInDatabase(2).addTaraWeight(w.getWeight());
-					
-					// 13 - Vægten tareres
-					d.setTaraWeight();
-					
-					// 14 - Operatøren instrueres i at placere netto (beholder med produkt)  på vægten
-					w.showLongMsg("Placer beholder med produkt på vægten"); 
-					
-					// 15 - Operatøren kvitterer
-					TimeUnit.SECONDS.sleep(3);
-					w.getInputWithMsg("Indtast 1, for at kvittere.");
-					
-					// 16 - Nettovægt registreres
-					d.batchInDatabase(2).addNettoWeight(addedWeight); //Den givne kg på vægten?
-					
-					// 17 - Vægten tareres
-					d.setTaraWeight();
-					
-					// 18 - Operatøren instrueres i at fjerne brutto fra vægten
-					w.showLongMsg("Fjern produkt fra vægt");
-					
-					// 19 - Operatøren kvitterer
-					TimeUnit.SECONDS.sleep	(3);
-					w.getInputWithMsg("Indtast 1, for at kvittere.");
-					
-					// 20 - Bruttovægt registreres (negativ)
-					d.batchInDatabase(2).addNettoWeight(d.batchInDatabase(2).getBruttoWeight()*-1);
-					
-					// 21 - Der udskrives OK eller kasseret på vægten
-					w.showMsg("ok!");
-					
-					// 22 - Operatøren kvitterer
-					TimeUnit.SECONDS.sleep(3);
-					w.getInputWithMsg("Indtast 1, for at kvittere.");
-					
-					// 23 - Vægten tareres
-					w.setTaraWeight();
-					
-					
-				} catch (WeightException e| InterruptedException e) {
+			int userInput = w.getInputWithMsg("Indtast operatørnummer");
+			w.showLongMsg(d.userInDatabase(userInput).getName());
+
+			// 3 - Operatørens navn findes i databasen og vises på vægten
+
+			// 4 - Operatøren kvitterer for at navnet er korrekt
+			TimeUnit.SECONDS.sleep(3);
+			w.getInputWithMsg("Indtast 1, for at kvittere.");
+
+			// 5 - Vægten beder om, at der indtastes batch nummer (område 1000-9999)
+			// 6 - Operatør indtaster batch nummer
+			int userBatch = w.getInputWithMsg("Indtast batch nummer.");
+			d.batchInDatabase(userBatch);
+
+			// 7 - Operatøren instrueres om, at vægten skal være ubelastet
+			w.showLongMsg("Fjern alt last fra vægten.");
+
+			// 8 - Operatøren kvitterer
+			TimeUnit.SECONDS.sleep(3);
+			w.getInputWithMsg("Indtast 1, for at kvittere.");
+
+			// 9 - Vægten tareres
+			w.setTaraWeight();
+
+			// 10 - Operatøren instrueres om, at placere tara (tom beholder)  på vægten7
+			w.showLongMsg("Placer en tom beholder på vægten");
+
+			// 11 - Operatøren kvitterer
+			TimeUnit.SECONDS.sleep(3);
+			w.getInputWithMsg("Indtast 1, for at kvittere.");
+
+			// 12 - Tara’s vægt registreres
+			d.batchInDatabase(2).addTaraWeight(w.getWeight());
+
+			// 13 - Vægten tareres
+			d.setTaraWeight();
+
+			// 14 - Operatøren instrueres i at placere netto (beholder med produkt)  på vægten
+			w.showLongMsg("Placer beholder med produkt på vægten"); 
+
+			// 15 - Operatøren kvitterer
+			TimeUnit.SECONDS.sleep(3);
+			w.getInputWithMsg("Indtast 1, for at kvittere.");
+
+			// 16 - Nettovægt registreres
+			d.batchInDatabase(2).addNettoWeight(addedWeight); //Den givne kg på vægten?
+
+			// 17 - Vægten tareres
+			d.setTaraWeight();
+
+			// 18 - Operatøren instrueres i at fjerne brutto fra vægten
+			w.showLongMsg("Fjern produkt fra vægt");
+
+			// 19 - Operatøren kvitterer
+			TimeUnit.SECONDS.sleep	(3);
+			w.getInputWithMsg("Indtast 1, for at kvittere.");
+
+			// 20 - Bruttovægt registreres (negativ)
+			d.batchInDatabase(2).addNettoWeight(d.batchInDatabase(2).getBruttoWeight()*-1);
+
+			// 21 - Der udskrives OK eller kasseret på vægten
+			w.showMsg("ok!");
+
+			// 22 - Operatøren kvitterer
+			TimeUnit.SECONDS.sleep(3);
+			w.getInputWithMsg("Indtast 1, for at kvittere.");
+
+			// 23 - Vægten tareres
+			w.setTaraWeight();
+
+
+		} catch (WeightException e| InterruptedException e) {
 			System.out.println(e);
 		}
 	}

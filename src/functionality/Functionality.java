fa45586e7e11f20dd67011d2e31d5e7a3e5fbe5b
Niklaes Jacobsen <niklaes@gmail.com>
2018-03-08 15:26:18 +0100

    try ændret i Funktionalitet

--- src/functionality/Functionality.java
+++ src/functionality/Functionality.java
@@ -1,13 +1,15 @@
 package functionality;
 
-import data.ObjectTransfer;
+import java.util.ArrayList;
+
+import data.*;
 
 public class Functionality {
 	private ObjectTransfer d;
 	private Weight_Translate w;
 	
 	public Functionality(){
-		d = new data.ObjectTransfer();
+		d = new ObjectTransfer();
 		w = new Weight_Translate();
 	}
 	
@@ -16,69 +18,58 @@ public class Functionality {
 	 */
 	public void weightFlow(){
 
-		try {
-		
-			w.showLongMsg("\"Sebastian Sebastiansen\"");
-			
-			w.getInputWithMsg("Indtast noget");
-			
-			w.removeMsg();
-			
-			System.out.println(w.getTaraWeight());
-			
-			w.closeAllLeaks();
+		try {		
+
+			// 1 - Vægten beder om, at der indtastes operatørnummer
+			// 2 - Operatøren indtaster sit brugernummer (område 11-99)
+				int userInput = w.getInputWithMsg("Indtast operatørnummer");
+					w.showLongMsg(d.isUserInDatabase(userInput));
+
+			// 3 - Operatørens navn findes i databasen og vises på vægten
+
+			// 4 - Operatøren kvitterer for at navnet er korrekt
+
+			// 5 - Vægten beder om, at der indtastes batch nummer (område 1000-9999)
+
+			// 6 - Operatør indtaster batch nummer
+
+			// 7 - Operatøren instrueres om, at vægten skal være ubelastet
+
+			// 8 - Operatøren kvitterer
+
+			// 9 - Vægten tareres
+
+			// 10 - Operatøren instrueres om, at placere tara (tom beholder)  på vægten
+
+			// 11 - Operatøren kvitterer
+
+			// 12 - Tara’s vægt registreres
+
+			// 13 - Vægten tareres
+
+			// 14 - Operatøren instrueres i at placere netto (beholder med produkt)  på vægten 
+
+			// 15 - Operatøren kvitterer
+
+			// 16 - Nettovægt registreres
+
+			// 17 - Vægten tareres
+
+			// 18 - Operatøren instrueres i at fjerne brutto fra vægten
+
+			// 19 - Operatøren kvitterer
+
+			// 20 - Bruttovægt registreres (negativ)
+
+			// 21 - Der udskrives OK eller kasseret på vægten
+
+			// 22 - Operatøren kvitterer
+
+			// 23 - Vægten tareres
+
 			
 		} catch (WeightException e) {
-			
 			System.out.println(e);
-			
 		}
-		
-		// 1 - Vægten beder om, at der indtastes operatørnummer
-
-		// 2 - Operatøren indtaster sit brugernummer (område 11-99)
-
-		// 3 - Operatørens navn findes i databasen og vises på vægten
-
-		// 4 - Operatøren kvitterer for at navnet er korrekt
-
-		// 5 - Vægten beder om, at der indtastes batch nummer (område 1000-9999)
-
-		// 6 - Operatør indtaster batch nummer
-
-		// 7 - Operatøren instrueres om, at vægten skal være ubelastet
-
-		// 8 - Operatøren kvitterer
-
-		// 9 - Vægten tareres
-
-		// 10 - Operatøren instrueres om, at placere tara (tom beholder)  på vægten
-
-		// 11 - Operatøren kvitterer
-
-		// 12 - Tara’s vægt registreres
-
-		// 13 - Vægten tareres
-
-		// 14 - Operatøren instrueres i at placere netto (beholder med produkt)  på vægten 
-
-		// 15 - Operatøren kvitterer
-
-		// 16 - Nettovægt registreres
-
-		// 17 - Vægten tareres
-
-		// 18 - Operatøren instrueres i at fjerne brutto fra vægten
-
-		// 19 - Operatøren kvitterer
-
-		// 20 - Bruttovægt registreres (negativ)
-
-		// 21 - Der udskrives OK eller kasseret på vægten
-
-		// 22 - Operatøren kvitterer
-
-		// 23 - Vægten tareres
-
 	}
 }

db821fd86f6da254ff52c95e6a19ea007a5f1aad
Niklaes Jacobsen <niklaes@gmail.com>
2018-03-08 16:12:51 +0100

    System wait

--- src/functionality/Functionality.java
+++ src/functionality/Functionality.java
@@ -24,10 +24,11 @@ public class Functionality {
 			// 2 - Operatøren indtaster sit brugernummer (område 11-99)
 			// 3 - Operatørens navn findes i databasen og vises på vægten
 				int userInput = w.getInputWithMsg("Indtast operatørnummer");
-					w.showLongMsg(d.isUserInDatabase(userInput));
+					w.showLongMsg(d.userInDatabase(userInput).getName());
 
 			// 4 - Operatøren kvitterer for at navnet er korrekt
-					TimeUnit.SECONDS.sleep(30);
+					TimeUnit.SECONDS.sleep(3);
+					w.getInputWithMsg("Indtast 1, for at kvittere.");
 					
 			// 5 - Vægten beder om, at der indtastes batch nummer (område 1000-9999)
 
@@ -36,14 +37,16 @@ public class Functionality {
 			// 7 - Operatøren instrueres om, at vægten skal være ubelastet
 
 			// 8 - Operatøren kvitterer
-					// - ------ wtf   ?????
+					TimeUnit.SECONDS.sleep(3);
+					w.getInputWithMsg("Indtast 1, for at kvittere.");
 					
 			// 9 - Vægten tareres
 
 			// 10 - Operatøren instrueres om, at placere tara (tom beholder)  på vægten
 
 			// 11 - Operatøren kvitterer
-					// - ------ wtf   ?????
+					TimeUnit.SECONDS.sleep(3);
+					w.getInputWithMsg("Indtast 1, for at kvittere.");
 					
 			// 12 - Tara’s vægt registreres
 
@@ -52,7 +55,8 @@ public class Functionality {
 			// 14 - Operatøren instrueres i at placere netto (beholder med produkt)  på vægten 
 
 			// 15 - Operatøren kvitterer
-					// - ------ wtf   ?????
+					TimeUnit.SECONDS.sleep(3);
+					w.getInputWithMsg("Indtast 1, for at kvittere.");
 					
 			// 16 - Nettovægt registreres
 
@@ -61,7 +65,8 @@ public class Functionality {
 			// 18 - Operatøren instrueres i at fjerne brutto fra vægten
 
 			// 19 - Operatøren kvitterer
-					// - ------ wtf   ?????
+					TimeUnit.SECONDS.sleep(3);
+					w.getInputWithMsg("Indtast 1, for at kvittere.");
 					
 			// 20 - Bruttovægt registreres (negativ)
 

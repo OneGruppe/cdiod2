52dccff7f20081d3fc948bf7e297638c81a4c122
Niklaes Jacobsen <niklaes@gmail.com>
2018-03-09 10:50:15 +0100

    Time sleep changed.

--- src/functionality/Functionality.java
+++ src/functionality/Functionality.java
@@ -16,80 +16,115 @@ public class Functionality {
 	 * Kører flow for vægten.
 	 */
 	public void weightFlow(){
-
+		int userInput;
+		int batchInput;
 		try {		
-			// 1 - Vægten beder om, at der indtastes operatørnummer
-			// 2 - Operatøren indtaster sit brugernummer (område 11-99)
-			int userInput;
-			userInput = w.getInputWithMsg("Indtast operatørnummer");
-			System.out.println(userInput);
-			// 3 - Operatørens navn findes i databasen og vises på vægten
-			w.showLongMsg(d.userInDatabase(userInput).getName());
+			while(true) {
+				// 1 - Vægten beder om, at der indtastes operatørnummer
+				// 2 - Operatøren indtaster sit brugernummer (område 11-99)
+				while (true) {
+					userInput = w.getInputWithMsg("Indtast operatørnummer");
+					System.out.println(userInput);
+					// 3 - Operatørens navn findes i databasen og vises på vægten
+					w.showLongMsg("Navn: " + d.userInDatabase(userInput).getName());
 
-			// 4 - Operatøren kvitterer for at navnet er korrekt
-			TimeUnit.SECONDS.sleep(3);
-			w.getInputWithMsg("Indtast 1, for at kvittere.");
+					// 4 - Operatøren kvitterer for at navnet er korrekt
+					TimeUnit.SECONDS.sleep(2);
+					int ok = w.getInputWithMsg("Tryk ok, for at bekræfte.");
+					if (ok == 0) {
+						break;
+					}
+				}
 
-			// 5 - Vægten beder om, at der indtastes batch nummer (område 1000-9999)
-			// 6 - Operatør indtaster batch nummer
-			int batchID = w.getInputWithMsg("Indtast batch nummer.");
-			d.batchInDatabase(batchID);
+				// 5 - Vægten beder om, at der indtastes batch nummer (område 1000-9999)
+				// 6 - Operatør indtaster batch nummer
+				while(true) {
+					batchInput = w.getInputWithMsg("Indtast batch nummer.");
+					d.batchInDatabase(batchInput);
 
-			// 7 - Operatøren instrueres om, at vægten skal være ubelastet
-			w.showLongMsg("Fjern alt last fra vægten.");
+					// 7 - Operatøren instrueres om, at vægten skal være ubelastet
+					w.showLongMsg("Batch :" + d.batchInDatabase(batchInput).getDesc());
+					TimeUnit.SECONDS.sleep(2);
+					w.showLongMsg("Fjern alt last fra vægten.");
 
-			// 8 - Operatøren kvitterer
-			TimeUnit.SECONDS.sleep(3);
-			w.getInputWithMsg("Indtast 1, for at kvittere.");
+					// 8 - Operatøren kvitterer
+					TimeUnit.SECONDS.sleep(2);
+					int ok = w.getInputWithMsg("Tryk ok, for at bekræfte.");
+					if (ok == 0) {
+						break;
+					}
+				}
 
-			// 9 - Vægten tareres
-			// 10 - Operatøren instrueres om, at placere tara (tom beholder)  på vægten
-			w.showLongMsg("Placer en tom beholder på vægten");
-			w.setTaraWeight();
+				// 9 - Vægten tareres
+				// 10 - Operatøren instrueres om, at placere tara (tom beholder)  på vægten
+				while (true) {
+					w.showLongMsg("Placer en tom beholder på vægten");
+					w.setTaraWeight();
 
-			// 11 - Operatøren kvitterer
-			TimeUnit.SECONDS.sleep(3);
-			w.getInputWithMsg("Indtast 1, for at kvittere.");
+					// 11 - Operatøren kvitterer
+					TimeUnit.SECONDS.sleep(2);
+					int ok = w.getInputWithMsg("Tryk ok, for at bekræfte.");
+					if (ok == 0) {
+						break;
+					}
+				}
 
-			// 12 - Tara’s vægt registreres
-			d.batchInDatabase(batchID).addTaraWeight(w.getWeight());
+				while(true) {
+					// 12 - Tara’s vægt registreres
+					d.batchInDatabase(batchInput).addTaraWeight(w.getWeight());
 
-			// 13 - Vægten tareres
-			w.setTaraWeight();
+					// 13 - Vægten tareres
+					w.setTaraWeight();
 
-			// 14 - Operatøren instrueres i at placere netto (beholder med produkt)  på vægten
-			w.showLongMsg("Placer beholder med produkt på vægten"); 
+					// 14 - Operatøren instrueres i at placere netto (beholder med produkt)  på vægten
+					w.showLongMsg("Placer beholder med produkt på vægten (5 sek.)"); 
 
-			// 15 - Operatøren kvitterer
-			TimeUnit.SECONDS.sleep(3);
-			w.getInputWithMsg("Indtast 1, for at kvittere.");
+					// 15 - Operatøren kvitterer
+					TimeUnit.SECONDS.sleep(5);
+					int ok = w.getInputWithMsg("Tryk ok, for at bekræfte.");
+					if (ok == 0) {
+						break;
+					}
+				}
 
-			// 16 - Nettovægt registreres
-			d.batchInDatabase(batchID).addNettoWeight(2); //Den givne kg på vægten?
+				while(true) {
+					// 16 - Nettovægt registreres
+					d.batchInDatabase(batchInput).addNettoWeight(2); //Den givne kg på vægten?
 
-			// 17 - Vægten tareres
-			w.setTaraWeight();
+					// 17 - Vægten tareres
+					w.setTaraWeight();
 
-			// 18 - Operatøren instrueres i at fjerne brutto fra vægten
-			w.showLongMsg("Fjern produkt fra vægt");
+					// 18 - Operatøren instrueres i at fjerne brutto fra vægten
+					w.showLongMsg("Fjern produkt fra vægt");
 
-			// 19 - Operatøren kvitterer
-			TimeUnit.SECONDS.sleep	(3);
-			w.getInputWithMsg("Indtast 1, for at kvittere.");
+					// 19 - Operatøren kvitterer
+					TimeUnit.SECONDS.sleep(2);
+					int ok = w.getInputWithMsg("Tryk ok, for at bekræfte.");
+					if (ok == 0) {
+						break;
+					}
+				}
 
-			// 20 - Bruttovægt registreres (negativ)
-			d.batchInDatabase(batchID).addNettoWeight(d.batchInDatabase(2).getBruttoWeight()*-1);
+				while(true) {
+					// 20 - Bruttovægt registreres (negativ)
+					d.batchInDatabase(batchInput).addNettoWeight(d.batchInDatabase(2).getBruttoWeight()*-1);
 
-			// 21 - Der udskrives OK eller kasseret på vægten
-			w.showMsg("ok!");
+					// 21 - Der udskrives OK eller kasseret på vægten
+					w.showMsg("ok!");
 
-			// 22 - Operatøren kvitterer
-			TimeUnit.SECONDS.sleep(3);
-			w.getInputWithMsg("Indtast 1, for at kvittere.");
+					// 22 - Operatøren kvitterer
+					TimeUnit.SECONDS.sleep(2);
+					int ok = w.getInputWithMsg("Tryk ok, for at bekræfte.");
+					if (ok == 0) {
+						break;
+					}
+				}
 
-			// 23 - Vægten tareres
-			w.setTaraWeight();
+				// 23 - Vægten tareres
+				w.setTaraWeight();
 
+				w.showLongMsg("This batch-process is done. \n Press '1' to close program, anything else to start over.");
+			}
 
 		} catch (WeightException | InterruptedException e) {
 			System.out.println(e);

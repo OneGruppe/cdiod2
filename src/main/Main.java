f4e51bcccf5a6de3abd8b93221806991f5dc604d
Niklaes Jacobsen <niklaes@gmail.com>
2018-03-19 12:24:17 +0100

    Physical weight changed

--- src/main/Main.java
+++ src/main/Main.java
@@ -16,10 +16,10 @@ public class Main {
 	 */
 	public static void main(String[] args){
 		// Actual weight (2)
-		// IFunctionality f = new Functionality_Psysical("169.254.2.3");
+		  IFunctionality f = new Functionality_Physical("169.254.2.3");
 
 		// Virtual weight
-		IFunctionality f = new Functionality_Virtual("127.0.0.1");
+		//IFunctionality f = new Functionality_Virtual("127.0.0.1");
 		f.weightFlow();
 	}
 
